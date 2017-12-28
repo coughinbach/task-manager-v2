7b96b5d4343ee46629f5856c233bec72149bec3b
Lola Barbier <lolabarbier@hotmail.com>
2017-12-28 15:19:25 +0100

    CRUD works

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -25,8 +25,26 @@ class TasksController < ApplicationController
   end
 
   def update
+    @task.update(task_params)
+    if @task.save
+      redirect_to tasks_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
