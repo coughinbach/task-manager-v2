4707f4dea45652fc18f6569ec91fcc98a1bafefa
Lola Barbier <lolabarbier@hotmail.com>
2017-12-28 15:01:26 +0100

    model done, started controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+p "creating tasks"
+Task.create(title: "Laundry", details: "Put clothes and detergent in washer, press buttons, have clean underwear once more.")
+Task.create(title: "Get groceries", details: "You have nothing to eat and are wasting away.")
+Task.create(title: "Work out", details: "Your twerpy arms look like tagliatella noodles and they make me sad. :(")
+p "done"
